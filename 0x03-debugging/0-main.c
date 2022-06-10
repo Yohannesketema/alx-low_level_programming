4fa14de46780383288b66e86fd93b39e31ef47d6
yohannesketema <ketemayohannes5@gmail.com>
2022-06-10 10:23:32 -0700

    0-main

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,13 +1,15 @@
 #include "main.h"
 /**
-* main
-* return (0)
+* main - test positive or negative
+* Return: 0
 */
+
 int main(void)
 {
-	int i = 0;
+	int i;
+	i = 0;
 
-	posetive_or_negative(i);
+	positive_or_negative(i);
 	return (0);
 }
 
