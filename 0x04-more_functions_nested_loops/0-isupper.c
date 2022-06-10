8deb13f1953e3a87df7898ad88c3fbe47b4a838d
yohannesketema <ketemayohannes5@gmail.com>
2022-06-10 07:50:07 -0700

    is upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * isupper is a function that verifies if a character is upeercase or not
+ * returns one if its upper case
+ */
+int _isupper(int c)
+{
+	if ((c >= 'A') && (c <= 'Z'))
+		return (1);
+}
+/*
+ * int main is the main function
+ *
+ * if its correct returns 0
+ */
+int main()
+{ 
+	_isupper(A);
+		return (0);
+}
