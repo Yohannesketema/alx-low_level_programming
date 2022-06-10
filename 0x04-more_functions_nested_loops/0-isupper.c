9594bc3cea030580d14f431ecc541dd6bcf5d47d
yohannesketema <ketemayohannes5@gmail.com>
2022-06-10 07:56:21 -0700

    is upper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -9,13 +9,3 @@ int _isupper(int c)
 	if ((c >= 'A') && (c <= 'Z'))
 		return (1);
 }
-/*
- * int main is the main function
- *
- * if its correct returns 0
- */
-int main()
-{ 
-	_isupper(A);
-		return (0);
-}
