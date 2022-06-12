c5359b61a3921a3ccc560285f2cebf3c4063a13d
yohannesketema <ketemayohannes5@gmail.com>
2022-06-11 23:29:07 -0700

    print lower case

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - print lower case alphabet
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		putchar('\n');
+		c++;
+	}
+	return (0);
+}
