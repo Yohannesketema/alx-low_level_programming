665c7971b27d1c021b3d9adcd50991d8c1792842
yohannesketema <ketemayohannes5@gmail.com>
2022-06-12 12:21:41 -0700

    times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -25,7 +25,7 @@ void print_times_table(int n)
 				if (a >= 100)
 				{
 					_putchar((a / 100) + '0');
-					_putchar((a % 10) + '0');
+					_putchar((a % 10) % 10 + '0');
 				}
 				else if (a <= 99 && a >= 10)
 				{
