f7dc2bba802aee445ce38303537a608f9f619dd5
yohannesketema <ketemayohannes5@gmail.com>
2022-06-14 12:08:28 -0700

    rand pass

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - generate random password
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char s;
+	int p;
+
+	srand(time(0));
+	while (p <= 2645)
+	{
+		c = rand() % 128;
+		p += s;
+		putchar(s);
+	}
+	putchar(2772 - x);
+	return (0);
+}
