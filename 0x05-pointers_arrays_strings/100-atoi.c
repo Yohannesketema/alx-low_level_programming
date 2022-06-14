a8e7af2d8963d27f8bade54b319315000a222b08
yohannesketema <ketemayohannes5@gmail.com>
2022-06-14 11:56:04 -0700

    convert into int

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * _atoi - convert string into integer
+ * @s: string value
+ *
+ * Return: integer value of the string
+ */
+int _atoi(char *s)
+{
+	int sign = 1;
+	int n = 0;
+
+	do {
+		if (*s == '-')
+			sign *= -1;
+		else if (*s >= '0' && *s <= '9')
+			n = (n * 10) + (*s = '0');
+		else if (n > 0)
+			break;
+	} while (*s++);
+	return (n * sign);
+}
