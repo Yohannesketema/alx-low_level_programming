166d4257c3526a31c0738d2cb00fae7debc66ec6
yohannesketema <ketemayohannes5@gmail.com>
2022-06-14 09:07:55 -0700

    print reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * print_rev - print string in reverse
+ * @s: holds string characters
+ */
+void print_rev(char *s)
+{
+	while (*str)
+	{
+		printf("%c", *str--);
+	}
+	printf("\n");
