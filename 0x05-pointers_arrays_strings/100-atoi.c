40addbb53ab7de0571cbe4a4bff4139a6e000412
yohannesketema <ketemayohannes5@gmail.com>
2022-06-14 11:58:16 -0700

    convert into int

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -14,7 +14,7 @@ int _atoi(char *s)
 		if (*s == '-')
 			sign *= -1;
 		else if (*s >= '0' && *s <= '9')
-			n = (n * 10) + (*s = '0');
+			n = (n * 10) + (*s - '0');
 		else if (n > 0)
 			break;
 	} while (*s++);
