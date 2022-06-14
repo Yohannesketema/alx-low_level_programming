687e45c8985422add60a51ef02ce91446c5b05b4
yohannesketema <ketemayohannes5@gmail.com>
2022-06-14 11:41:45 -0700

    str copy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,12 +9,11 @@ char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
 
-	while (*(src + i) != '\0')
+	while (src[i])
 	{
-		*(dest + 1) = *(src + 1);
+		dest[i] = src[i];
 		i++;
 	}
-	*(dest + i) = '\0';
 	return (dest);
 }
 
