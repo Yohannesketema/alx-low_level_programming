cfdf47560ee76750edda348b3898c7abed46d5eb
yohannesketema <ketemayohannes5@gmail.com>
2022-06-15 23:07:55 -0700

    main correction

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
 *_isdigit - input
  * Description: define if a character is a number
@@ -9,7 +8,7 @@
 
 int _isdigit(int c)
 {
-	if _isdigit(int c)
+	if (c .= '0' && c <= '9')
 		return (1);
 	else
 		return (0);
