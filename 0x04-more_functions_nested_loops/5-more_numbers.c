8115cca44c7e223d3f5256179f2a3c3a083ceaa1
yohannesketema <ketemayohannes5@gmail.com>
2022-06-15 23:43:19 -0700

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * more_numbers - print 1 - 14 ten times
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i < 9; i++)
+	{
+		for (j = 0; j <= 14; j++)
+			_putchar(j + '0');
+		_putchar('\n');
+	}
+}
