a7d7aaf8fbef8c90328fc11763de40c1560bb8fd
yohannesketema <ketemayohannes5@gmail.com>
2022-06-16 07:40:54 -0700

    concatenate

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,6 +8,11 @@
  */
 char _strcat(char *dest, char *src)
 {
-	strcat(*dest, *src);
+	int i = 0, d_l = 0;
+
+	while (dest[i++])
+		d_l++;
+	for (i = 0; src[i]; i++)
+		dest[d_l++] = src[i];
 	return (*dest);
 }
