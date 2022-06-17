fd7227a9dadf821c171ced6eb3b43ad548f1dc2f
yohannesketema <ketemayohannes5@gmail.com>
2022-06-16 20:24:33 -0700

    line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#inckude "main.h"
+/**
+ * print_line - print line
+ * @n: length of the line
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
