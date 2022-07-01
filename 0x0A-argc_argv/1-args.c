87703ca8c64fe4ae018c71010334b97cffc2e405
yohannesketema <ketemayohannes5@gmail.com>
2022-07-01 01:19:11 -0700

    number

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - prints the argument passed to it
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
