614cef12af5f35becdbf5c315cab55f080d4f907
yohannesketema <ketemayohannes5@gmail.com>
2022-07-01 00:59:28 -0700

    prog name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,8 +5,8 @@
  * @argv: argument vector
  * Return: 0
  */
-int main(int __attribute__((__unused__)) argc, char *argv[])
+int main(int argc, char *argv[])
 {
-	printf("%s\n", argv[0]);
+	printf("%s\n", *argv[0]);
 	return (0);
 }
